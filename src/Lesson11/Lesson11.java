c25c42710f7e7e4753499409cd9996c6435564e5
AnatoliyPetukh <k04375@gmail.com>
2023-01-08 18:43:52 +0100

    Курсы Java Lesson11

---
+++ src/Lesson11/Lesson11.java
@@ -0,0 +1,38 @@
+package Lesson11;
+
+public class Lesson11{
+static class Car {
+    String Color;
+    String Engine;
+    int door;
+    public Car(String Color,String Engine, int door){
+        this.Color=Color;
+        this.Engine=Engine;
+        this.door=door;
+    }
+}
+
+ static class CarTest{
+    void changeDoor(Car c1, int d){
+    c1.door=d;
+    }
+    void changeColor(Car a, Car b){
+        String c=b.Color;
+        b.Color=a.Color;
+        a.Color=c;
+    }
+}
+
+    public static void main(String[] args) {
+    CarTest ct = new CarTest();
+        Car car1 = new Car("black","V6",4);
+        Car car2 = new Car("white","V8",2);
+        ct.changeDoor(car1,2);
+        ct.changeDoor(car2,4);
+        System.out.println(car1.door);
+        System.out.println(car2.door);
+        ct.changeColor(car1,car2);
+        System.out.println(car1.Color);
+        System.out.println(car2.Color);
+    }
+}
--- src/Lesson8/STATIC_FINAL.java
+++ src/Lesson8/STATIC_FINAL.java
@@ -1,9 +1,31 @@
 package Lesson8;
-import java.util.Scanner;
-public class STATIC_FINAL {
 
+import java.util.Scanner;
+
+ class Lesson8{
+    int a=2;
+    static int a22=2;
+
+}
+  /* public final static double Phi=3.14;
+   int r;
+   int l;
+
+    public double S (int r2){
+        r=Phi*r2*r2;
+        return r;
+    }
+    public double L (int r2) {
+        l = 2 * Phi * r2;
+        return l;
+    }
+    public void (int r2){
+        r=r2;
+        System.out.println(r2; radius(4);dlina(4) );
+    }
     String name;
     //Scanner input = new Scanner(System.in);
 //System.out.println()
 }
 
+*/
