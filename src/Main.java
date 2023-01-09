f302518563c9bd6c295dceb6cb3f323d1d55d59d
AnatoliyPetukh <k04375@gmail.com>
2023-01-09 22:03:17 +0100

    Курсы Java Lesson11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}*/
+
+package com.javarush.task.pro.task11.task1113;
+
+/*
+Следим за скобками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected");
+        }
     }
 }
